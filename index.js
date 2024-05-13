52fb532bb4baacc7e23f178a031dbdc5245c1e68
nicole wachter <nicole.wachter6@gmail.com>
2024-05-13 17:51:45 -0400

    ADDs looping-code

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let thankYou = []
+    for (let i = 0; i < array.length; i++) {
+        thankYou.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        }
+        return thankYou;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log (number);
+        number -= 1;
+    }
+
+}
